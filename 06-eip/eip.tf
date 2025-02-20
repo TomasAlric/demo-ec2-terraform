ffcce9b99500ac3cedc9e3a424061990d8aec357
TomasAlric <tomas.alricc@gmail.com>
2025-02-20 08:51:43 -0300

    fix: frontend id

--- 06-eip/eip.tf
+++ 06-eip/eip.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "frontend_eip" {}
 
 resource "aws_eip_association" "frontend_association" {
-  instance_id   = data.aws_instance.frontend.id  
+  instance_id   = data.terraform_remote_state.frontend.outputs.frontend_instance_id 
   allocation_id = aws_eip.frontend_eip.id  
 }
--- 06-eip/locals.tf
+++ 06-eip/locals.tf
@@ -2,6 +2,4 @@ locals {
   tags = merge(var.tags, {
     Environment = "production"
   })
-
-  frontend_instance_id = data.terraform_remote_state.frontend.outputs.frontend_instance_id
 }
