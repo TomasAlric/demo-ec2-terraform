9e6a52a9792e5425e54b3c9d85cb0488bff31fdd
TomasAlric <tomas.alricc@gmail.com>
2025-02-20 04:51:29 -0300

    fix: include secrets

--- 02-utils/secrets.tf
+++ 02-utils/secrets.tf
@@ -3,4 +3,13 @@ resource "aws_secretsmanager_secret" "database_secrets" {
   description = "Revolution database credentials"
 
   recovery_window_in_days = 0
+  
+}
+
+resource "aws_secretsmanager_secret_version" "database_secrets_version" {
+  secret_id     = aws_secretsmanager_secret.database_secrets.id
+  secret_string = jsonencode({
+    username = "admin"
+    password = "password123"
+  })
 }
